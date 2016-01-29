0a5fe6451ff399808036157043ab2bae176a7bf4
albert <albert@spiritframe.com>
2016-01-30 02:05:46 +0800

    init: VIP

--- tests/client.go
+++ tests/client.go
@@ -4,22 +4,39 @@ import (
 	"golang.org/x/net/websocket"
 	"log"
 	"time"
-	"fmt"
+	"net"
 )
 
-func connect() {
-	origin := "http://127.0.0.1/"
-	url := "ws://127.0.0.1:8080/ws"
-	_, err := websocket.Dial(url, "", origin)
-	if err != nil {
-		log.Fatal(err)
+func connect(localip string) {
+	origin := "http://192.168.1.150/"
+	url := "ws://192.168.1.150:8080/ws"
+	//ws, err = websocket.Dial(url, "", origin)
+	config,err := websocket.NewConfig(url, origin)
+	if err != nil{
+		log.Println(err)
+		return
 	}
+	localaddr := &net.TCPAddr{IP:net.ParseIP(localip)}
+	remoteaddr := &net.TCPAddr{IP:net.ParseIP("192.168.1.150"),Port:8080}
+	client,err := net.DialTCP("tcp4",localaddr,remoteaddr)
+	if err != nil{
+		log.Println(err)
+		return
+	}
+	_, err = websocket.NewClient(config,client)
+	if err != nil {
+		log.Println(err)
+	}
+	time.Sleep(time.Second*100)
+	return
 }
 
 func main() {
-	for i := 0; i < 100000; i++ {
-		go connect()
-		fmt.Println(i)
+	for i := 0; i < 65535; i++ {
+		go connect("192.168.1.101")
+		go connect("192.168.1.102")
+		go connect("192.168.1.150")
+		log.Println(i)
 	}
 	time.Sleep(time.Second * 100)
 }
--- tests/service.go
+++ tests/service.go
@@ -5,17 +5,22 @@ import (
 
 	"golang.org/x/net/websocket"
 	"fmt"
+	"io"
 )
 
+var totle int
+var b []byte
 // Echo the data received on the WebSocket.
 func EchoServer(ws *websocket.Conn) {
-	fmt.Println(ws.Request().Header)
+	totle ++
+	fmt.Println(totle, ws.LocalAddr())
+	io.Copy(ws,ws)
 }
 
 // This example demonstrates a trivial echo server.
 func main() {
 	http.Handle("/ws", websocket.Handler(EchoServer))
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe("192.168.1.150:8080", nil)
 	if err != nil {
 		panic("ListenAndServe: " + err.Error())
 	}
